895b20fe5b77fa06fc45cf3c75007c954ee0660c
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-10-09 13:57:01 -0300

    PR-6 boilerplate

---
+++ src/databaseFunctions.py
@@ -0,0 +1,11 @@
+from pony.orm import *
+#from pony import orm
+
+db = Database()
+
+# class definitions
+
+# Example:
+#	class User(db.Entity):
+#		name = Required(str)
+	
---
+++ src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from databaseFunctions import *
+
+app = FastAPI()
+
+# TODO: implementation
+@app.get("/")
+async def root():
+	pass
+
+# TODO: add endpoints
+
