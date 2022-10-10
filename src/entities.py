7e953d65cc75c5b6a6d6effc71130885efa63258
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-10-10 14:02:23 -0300

    PR-37 archivo entities agregado

---
+++ src/entities.py
@@ -0,0 +1,15 @@
+from pony.orm import *
+
+db = Database()
+
+# class definitions
+
+class User(db.Entity):
+    username = Required(str, unique=True)
+    password = Required(str)
+    email = Required(str, unique=True)
+    is_confirmed = Required(bool, default=False, sql_default='0')
+    avatar = Optional(str)
+
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
