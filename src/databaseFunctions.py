ed41fe3d6c309fb45d1d6cefb74b84c13cfb899e
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-10-10 13:40:07 -0300

    PR-9 implementacion entidad usuario

--- src/databaseFunctions.py
+++ src/databaseFunctions.py
@@ -5,7 +5,12 @@ db = Database()
 
 # class definitions
 
-# Example:
-#	class User(db.Entity):
-#		name = Required(str)
-	
+class User(db.Entity):
+    username = Required(str, unique=True)
+    password = Required(str)
+    email = Required(str, unique=True)
+    is_confirmed = Required(bool, default=False, sql_default='0')
+    avatar = Optional(str)
+
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
