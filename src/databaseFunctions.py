c24ff937782690cf67078166aba6d40d1e237bf6
Ángeles Carrara <angeles.carrara@mi.unc.edu.ar>
2022-10-11 15:07:46 -0300

    endpoint implementado que necesita revisiones/tests

--- src/databaseFunctions.py
+++ src/databaseFunctions.py
@@ -1,9 +1,19 @@
 from pony.orm import *
 from entities import *
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# function definitions
 
-# Example: 
-#   def show_users():
-#       with db_session:
-#           User.select().show()
+@db_session
+def get_all_usernames():
+    select(u.username for u in User)[:]
+
+@db_session
+def get_all_emails():
+    select(u.email for u in User)[:]
+
+@db_session
+def upload_user(username: str, password: str,
+                email: str, avatar: Optional(str)) -> int:
+    u = User(username=username, password=generate_password_hash(password),
+         email=email, avatar=avatar)
+    return u.id
--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
-from databaseFunctions import *
+from fastapi import FastAPI, HTTPException, status
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+from databaseFunctions import get_all_usernames, get_all_emails, upload_user
 
 
 app = FastAPI()
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    avatar: Optional[str] = None
+
+class UserOut(BaseModel):
+    id: int
+    operation_result: str
 
 # TODO: implementation
 @app.get("/")
@@ -10,4 +22,26 @@ async def root():
 	pass
 
 # TODO: add endpoints
+
+# create a user: registro de usuario
+@app.post(
+    "/users/",
+    response_model=UserOut,
+    status_code=status.HTTP_201_CREATED
+)
+async def create_user(new_user: UserIn) -> int:
+    if new_user.username in get_all_usernames():
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, 
+            detail="A user with this username already exists"
+        )
+    if new_user.email in get_all_emails():
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, 
+            detail="A user with this email already exists"
+        )
+    return UserOut(
+        id = upload_user(new_user.username, new_user.password,
+                new_user.email, new_user.avatar),
+        operation_result="Succesfully created!")
 
