83235309d1e284e5d08ae1073f20e6895bead1c2
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-10-12 12:46:56 -0300

    PR-40 solucionado errores sintacticos y falta de atributos/relaciones

--- src/entities.py
+++ src/entities.py
@@ -11,6 +11,8 @@ class User(db.Entity):
     email = Required(str, unique=True)
     is_confirmed = Required(bool, default=False, sql_default='0')
     avatar = Optional(str)
+    robots = Set('Robot') 
+    matches = Set('Match')
     
 class Match(db.Entity):
     id = PrimaryKey(int, auto=True)
@@ -20,15 +22,16 @@ class Match(db.Entity):
     number_of_games = Required(int)
     password = Optional(str)
     is_finished = Required(bool, default=False, sql_default='0')
+    users = Set(User)
     
 
 class Robot(db.Entity):
     id = PrimaryKey(int,auto=True) # Clave primaria, no se si hace falta, capaz q si por la relacion
-    user = Required(User) 
+    user = Optional(User)
     name = Required(str)
     avatar = Optional(str)
-    behavior_file = Required(str)
-    matches_played = Required (int,default=0) 
+    behaviour_file = Required(str)
+    matches_played = Required (int,default=0)
     matches_won = Required(int, default=0)
     matches_lost = Required(int, default=0)
     matches_drawed = Required(int, default=0)
