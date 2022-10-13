68a5bed874c2eaf59287a76ec48d08410252fbd7
Estebanjabad <estebanjabad@gmail.com>
2022-10-13 11:27:39 -0300

    Implementación parcial de crear partida

--- src/databaseFunctions.py
+++ src/databaseFunctions.py
@@ -1,9 +1,31 @@
 from pony.orm import *
-from entities import *
+from entities import User, Match, Robot, db
 
-# function definitions
+"""
+        Function definitions:
+"""
 
-# Example: 
-#   def show_users():
-#       with db_session:
-#           User.select().show()
+
+
+
+
+
+# Creates a new Match and returns it's id.
+def match_add(
+        name_in: str,
+        max_players_in: int,
+        min_players_in: int,
+        number_of_games_in: int,
+        password_in: str,
+        creator_id_in: int
+    ):
+    with db_session:
+        #TODO En la siguiente línea solía decir user_id en lugar de user_id_in
+        creator_user = get(u for u in User if u.id==creator_id_in)
+        new_match = Match(name=name_in,
+                        max_players=max_players_in,
+                        min_players=min_players_in,
+                        number_of_games=number_of_games_in,
+                        password=password_in,
+                        creator=creator_id_in)
+        return new_match.id
--- src/main.py
+++ src/main.py
@@ -1,13 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from databaseFunctions import *
-
+from entities import User, Match, Robot
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
+
+class MatchIn(BaseModel): 
+    name: str
+    max_players: int
+    min_players: int
+    number_of_games: int
+    password: str
+    creator_id: int
+
+class MatchOut(BaseModel):
+	match_id: int
+	operation_result: str
 
 # TODO: implementation
 @app.get("/")
 async def root():
 	pass
 
-# TODO: add endpoints
+    
 
+"""
+	Registrar robot:
+"""
+@app.post(
+	"/matches/",
+	response_model=MatchOut,
+	status_code=status.HTTP_201_CREATED
+)
+async def create_match(new_match: MatchIn) -> int:
+	creator= get_user_by_id(MatchIn.creator_id)
+	match_id = match_add(MatchIn.name,
+                        MatchIn.max_players,
+                        MatchIn.min_players,
+                        MatchIn.number_of_games,
+                        MatchIn.password,
+                        creator)
+	return MatchOut(
+		new_match_id = match_id,
+		operation_result = "Match succesfully created"
+	)
