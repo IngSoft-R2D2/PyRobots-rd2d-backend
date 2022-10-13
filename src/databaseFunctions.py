73c0e9ef1ed071005f398b97a7994d53aa72078f
Benjamin Eduardo Alvarez <benjamin.alvarez@mi.unc.edu.ar>
2022-10-13 11:05:15 -0300

    funcionando, falta tests

--- src/databaseFunctions.py
+++ src/databaseFunctions.py
@@ -1,6 +1,7 @@
 from pony.orm import *
 from entities import *
-
+from pydantic import BaseModel
+#from functions import * (eliminar)
 # function definitions
 
 # Example: 
@@ -8,7 +9,21 @@ from entities import *
 #       with db_session:
 #           User.select().show()
 
+class MatchInDB(BaseModel):
+    id: int
+    name: str
+    max_players: int
+    min_players: int
+    number_of_games: int 
+    is_finished: bool
+
+    class Config:
+    	orm_mode = True
+
 @db_session
 
 def get_all_matches ():
-	return (select (m for m in Match).show())
+	with db_session:
+		matches = Match.select()
+		result = [MatchInDB.from_orm(m) for m in matches]
+	return (result)
--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 from databaseFunctions import *
-
+from functions import *
+from entities import * 
 
 app = FastAPI()
+
 
 # TODO: implementation
 @app.get("/")
 async def root():
 	pass
 
-def get_all_matches ():
-	
-	return
 # TODO: add endpoints
 
 #listar partidas 
 @app.get("/match/")
-async def show_all_matches():
-	return get_all_matches()
+async def show_all_matches(): 
+	return (get_all_matches())
