738e1cc523ce1c603baa4527327d8f3a3cada571
Matias Teruel <85464085+matiteruel@users.noreply.github.com>
2022-10-15 15:51:20 -0300

    Revert "Feature pr 22 implementar endpoint para login"

--- src/databaseFunctions.py
+++ src/databaseFunctions.py
@@ -1,29 +1,9 @@
 from pony.orm import *
 from entities import *
-from typing import Optional
-from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# function definitions
 
-@db_session
-def get_user_by_username(username: str):
-    return User.get(username=username)
-
-@db_session
-def authenticate_user(username: str, password: str):
-    user = get_user_by_username(username)
-    if not user:
-        return False
-    if not pwd_context.verify(password, user.password):
-        return False
-    return user
-
-@db_session
-def upload_user(username: str, password: str,
-                email: str, avatar: Optional[str]):
-    if avatar is None:
-        User(username=username, password=pwd_context.hash(password),
-             email=email)
-    else:
-        User(username=username, password=pwd_context.hash(password),
-        email=email, avatar=avatar)
+# Example: 
+#   def show_users():
+#       with db_session:
+#           User.select().show()
