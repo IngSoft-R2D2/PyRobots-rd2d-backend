51050b7cf3f148eec39ce25d68cfd2d6d9096a2b
angelescch <angeles.carrara@mi.unc.edu.ar>
2022-10-31 00:04:40 -0300

    scann en el primer cuadrante

--- src/robot.py
+++ src/robot.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from asyncio.windows_events import INFINITE
+from turtle import distance
 from constants import *
 import random
 import math
@@ -8,6 +11,9 @@ class Robot:
     __position: tuple[float,float]
     __damage: int
     __wall_collision: bool
+    __scanner_direction: int
+    __resolution: int
+    __scann_result: float
 
     def __init__(self):
         self.__position = (500,500)#(random.randint(FIRST_COORD,LAST_COORD), random.randint(FIRST_COORD,LAST_COORD))
@@ -24,6 +30,13 @@ class Robot:
 
     def get_damage(self):
         return self.__damage
+
+    def point_scanner(self, direction: int, resolution: int):
+        self.__scanner_direction = direction
+        self.__resolution = resolution
+
+    def scanned(self):
+        return self.__scann_result
 
     def drive(self, direction: int, velocity: int):
         self.direction = direction
@@ -78,3 +91,16 @@ class Robot:
             x_axis = 999
             __wall_collision = True
         self.__position = (x_axis,y_axis)
+
+    def __scann(self, list_of_robots: list[Robot]):
+        dist = float('inf')
+        for robot in list_of_robots:
+            (x1,y1) = self.get_position()
+            (x2,y2) = robot.get_position()
+            m = (y2-y1)/(x2-x1)
+            theta = math.degrees(math.atan(m))
+            if (self.__scanner_direction-self.__resolution <= theta <= self.__scanner_direction+self.__resolution):
+                d =  math.sqrt((x2-x1)**2+(y2-y1)**2)
+                if d < dist:
+                    dist = d
+        self.__scann_result = dist
