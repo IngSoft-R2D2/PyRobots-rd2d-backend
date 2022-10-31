8f33fd876c42f962a55cf661e9a040fc617233bc
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-10-30 23:27:48 -0300

    constantes necesarias

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,15 @@
-# Max velocity (in meters) per round
+# Max velocity per round (meters)
 VELOCITY = 50
 
-# Board dimensions
+# Board dimensions (meters)
 FIRST_COORD = 0
 LAST_COORD = 999
+
+# Ranges (meters)
+CANNON_RANGE = 700
+
+# Damages (percentages)
+COLISSION_DAMAGE = 2
+MISSILE_DAMAGE_40_METERS = 3
+MISSILE_DAMAGE_20_METERS = 5
+MISSILE_DAMAGE_5_METERS = 10
