6a69ba670308a1663f997b6183de26df840d70c7
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-11-02 16:43:26 -0300

    fake_simulation result

--- src/main.py
+++ src/main.py
@@ -514,6 +514,179 @@ async def leave_match(
         )
 
 
+fk_simulation = simulation = """{
+                                    'round_1': {
+                                            'R1': {
+                                                'damage':0,
+                                                'position': (300,500),
+                                                'missile': (450,500)
+                                            },
+                                            'R2': {
+                                                'damage':0,
+                                                'position': (100,990),
+                                                'missile': (300,500)
+                                            },
+                                            'R3': {
+                                                'damage':0,
+                                                'position': (276,893),
+                                                'missile': (328,582)
+                                            },
+                                            'R4': {
+                                                'damage':0,
+                                                'position': (832,923),
+                                                'missile': (302,623)
+                                            }
+                                            },
+                                    'round_2': {
+                                                'R1': {
+                                                    'damage':30,
+                                                    'position': (349,567),
+                                                    'missile': (456,523)
+                                                },
+                                                'R2': {
+                                                    'damage':8,
+                                                    'position': (189,923),
+                                                    'missile': (256,736)
+                                                },
+                                                'R3': {
+                                                    'damage':0,
+                                                    'position': (375,789),
+                                                    'missile': (423,824)
+                                                },
+                                                'R4': {
+                                                    'damage':0,
+                                                    'position': (782,903),
+                                                    'missile': (812,989)
+                                                }
+                                            },
+                                    'round_3': {
+                                                'R1': {
+                                                'damage':33,
+                                                'position': (298,599),
+                                                'missile': (423,550)
+                                                },
+                                                'R2': {
+                                                'damage':26,
+                                                'position': (50,989),
+                                                'missile': (305,489)
+                                                },
+                                                'R3': {
+                                                'damage':16,
+                                                'position': (256,878),
+                                                'missile': (300,500)
+                                                },
+                                                'R4': {
+                                                'damage':5,
+                                                'position': (817,878),
+                                                'missile': (278,623)
+                                                }
+                                            },
+                                    'round_4': {
+                                            'R1': {
+                                                'damage':93,
+                                                'position': (322,480),
+                                                'missile': (450,500)
+                                            },
+                                            'R2': {
+                                                'damage':48,
+                                                'position': (189,999),
+                                                'missile': (500,473)
+                                            },
+                                            'R3': {
+                                                'damage':22,
+                                                'position': (312,872),
+                                                'missile': (289,645)
+                                            },
+                                            'R4': {
+                                                'damage':20,
+                                                'position': (777,956),
+                                                'missile': (482,734)
+                                            }
+                                        },
+                                    'round_5': {
+                                            'R2': {
+                                            'damage':58,
+                                            'position': (232,902),
+                                            'missile': (689,599)
+                                            },
+                                            'R3': {
+                                            'damage':33,
+                                            'position': (345,900),
+                                            'missile': (467,745)
+                                            },
+                                            'R4': {
+                                            'damage':28,
+                                            'position': (789,723),
+                                            'missile': (323,897)
+                                            }
+                                        },
+                                    'round_6': {
+                                        'R2': {
+                                            'damage':60,
+                                            'position': (245,972),
+                                            'missile': (989,399)
+                                        },
+                                        'R3': {
+                                            'damage':40,
+                                            'position': (345,900),
+                                            'missile': (467,745)
+                                        },
+                                        'R4': {
+                                            'damage':30,
+                                            'position': (789,723),
+                                            'missile': (323,897)
+                                        }
+                                        },
+                                    'round_7': {
+                                        'R2': {
+                                            'damage':75,
+                                            'position': (308,952),
+                                            'missile': (723,498)
+                                        },
+                                        'R3': {
+                                            'damage':50,
+                                            'position': (333,922),
+                                            'missile': (582,865)
+                                        },
+                                        'R4': {
+                                            'damage':35,
+                                            'position': (800,700),
+                                            'missile': (300,912)
+                                        }
+                                        },
+                                    'round_8': {
+                                        'R3': {
+                                            'damage':56,
+                                            'position': (356,944),
+                                            'missile': (629,999)
+                                        },
+                                        'R4': {
+                                            'damage':45,
+                                            'position': (832,712),
+                                            'missile': (343,923)
+                                        }
+                                        },
+                                    'round_9': {
+                                        'R3': {
+                                            'damage':98,
+                                            'position': (367,956),
+                                            'missile': (429,599)
+                                        },
+                                        'R4': {
+                                            'damage':50,
+                                            'position': (832,712),
+                                            'missile': (343,923)
+                                        }
+                                        },
+                                    'round_10': {
+                                        'R4': {
+                                            'damage':60,
+                                            'position': (832,712)
+                                        }
+                                        },
+                                    }"""
+
+
 """
     Get simulation.
 """
@@ -536,6 +709,6 @@ async def start_simulation(
         robots=robots_for_game
     )
     return SimulationOut(
-        simulation_json=rounds,
+        simulation_json=fk_simulation,
         operation_result="Simulation successfully runned."
     )
